e9d4639004fe54ef5b109572f352c57c2798abf8
Devi Prasad <78023117+prasaddevi907@users.noreply.github.com>
2024-01-24 12:18:15 +0530

    Delete crudAPI directory

--- crudAPI/src/main/java/com/crud/crudAPI/dao/CoureceDao.java
+++
@@ -1,9 +0,0 @@
-package com.crud.crudAPI.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.crud.crudAPI.entity.Cource;
-
-public interface CoureceDao extends JpaRepository<Cource, Long> {
-
-}
